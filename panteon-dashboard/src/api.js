eae2de7befbf1cff1738b9e705e4d70e86d64848
Ziya Eren Atik <69755628+erenatikk@users.noreply.github.com>
2024-08-01 13:35:37 +0300

    Update api.js

--- panteon-dashboard/src/api.js
+++ panteon-dashboard/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:5100",
+  baseURL: "https://3.120.205.237:5100",
 });
 
 export const login = (credentials) => {
